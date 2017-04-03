0f86ac209f51e1d2bed7021e617dec656024723d
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-04-02 23:20:13 -0500

    Add more list pattern tests

--- tests/test-files/good/Cases/ListPattern.elm
+++ tests/test-files/good/Cases/ListPattern.elm
@@ -1,6 +1,22 @@
+module Main exposing (..)
+
 
 length list =
-  case list of
-    []        -> 0
-    [a]       -> 1
-    a :: rest -> 2
+    case list of
+        [] ->
+            0
+
+        [ a ] ->
+            1
+
+        a :: rest ->
+            2
+
+
+firstWithDefault list default =
+    case list of
+        [] ->
+            default
+
+        a :: _ ->
+            a

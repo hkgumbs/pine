141fababf12c6352064cefbc4b405466d87068a9
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-04-18 23:28:20 -0500

    Remove more TDD artifacts

--- tests/test-files/good/Cases/ListPattern.elm
+++ tests/test-files/good/Cases/ListPattern.elm
@@ -1,22 +1,6 @@
-module Main exposing (..)
 
-
-length list =
-    case list of
-        [] ->
-            0
-
-        [ a ] ->
-            1
-
-        a :: rest ->
-            2
-
-
-firstWithDefault list default =
-    case list of
-        [] ->
-            default
-
-        a :: _ ->
-            a
+fakeLength list =
+  case list of
+    [] -> 0
+    [a] -> 1
+    a :: rest -> 2
